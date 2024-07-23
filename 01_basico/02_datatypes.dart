edc224848bd15143de13baeb58e60fe5253ab3f9
manucastelnovo <marnutskate@gmail.com>
2024-07-23 15:55:48 -0400

    final data type

--- 01_basico/02_datatypes.dart
+++ 01_basico/02_datatypes.dart
@@ -1,34 +1,79 @@
-void main() {
-  // numeros
-
+main() {
+  // ===== Números
   int a = 10;
   double b = 5.5;
 
-  int? c = null;
-  print(c);
+  int? c;
 
-  // string
+  int _a = 30;
+  double $b = 40;
 
-  String nombre = 'tony';
-  String? nombre2;
-  String interpolacion = '$nombre sii';
-  print(nombre2);
-  print(interpolacion);
+  double resultado = _a + $b;
 
-  //booleans
+  // print( resultado );
 
+  // ===== String
+
+  String nombre = 'Tony';
+  String nombre2 = "Tony";
+  String nombre3 = "O'Connor";
+  String apellido = 'Stark';
+
+  String nombreCompleto = '$nombre $apellido';
+
+  String multilinea = '''
+  Hola Mundo
+  ¿Cómo estás?
+  $nombreCompleto
+  O'Connor''';
+
+  // print(multilinea);
+
+  // ===== Booleans
   bool isActive = true;
   bool isNotActive = !isActive;
-  print(isNotActive);
 
-  //list
+  // print( isNotActive );
 
-  List<String> villanos = ['pepe', 'jeje', 'dart'];
+  // ===== Lists
+  // List<String> villanosDeprecated = new List();
+  List<String> villanos = ['Lex', 'Red Skull', 'Doom']; // Son base 0
+  //                         0        1         2
 
-  villanos[0] = 'jeje';
-  print(villanos);
+  villanos.add('Duende Verde');
+  villanos.add('Duende Verde');
+  villanos.add('Duende Verde');
+  villanos.add('Duende Verde');
 
-  //sets
-  Set<String> villanos2 = {'let', 'pepe', 'perro'};
-  print(villanos2);
+  // print( villanos );
+
+  var villanosSet = villanos.toSet();
+  // print(villanosSet.toList());
+
+  // ======== Sets
+  Set<String> villanos2 = {'Lex', 'Red Skull', 'Doom'};
+
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+  villanos2.add('Duende Verde');
+
+  // print( villanos2 );
+
+  // ====== Maps            Diccionarios / Objetos literales
+  Map<int, dynamic> ironman = {
+    1: 'Tony Stark',
+    2: 'Inteligencia y el dinero',
+    3: 9000,
+  };
+
+  // print( ironman[3] )
+  Map<String, dynamic> capitan = new Map();
+
+  capitan.addAll(
+      {'nombre': 'Steve', 'poder': 'Soportar droga sin morir', 'nivel': 5000});
+  // capitan.addAll(ironman);
+
+  print(capitan);
 }
