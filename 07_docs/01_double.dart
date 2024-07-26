4d92741b3a460137c186d70e9d3077f2e0af35f1
manucastelnovo <marnutskate@gmail.com>
2024-07-26 14:46:18 -0400

    http dart

---
+++ 07_docs/01_double.dart
@@ -0,0 +1,5 @@
+void main(List<String> args) {
+  double numero = 3.14;
+  double infinito = double.maxFinite ;
+  print(infinito);
+}
---
+++ http-dart/http_dart/bin/http_dart.dart
@@ -0,0 +1,5 @@
+import 'package:http_dart/http_dart.dart' as http_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${http_dart.calculate()}!');
+}
